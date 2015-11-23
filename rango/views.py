be12d8c9f0d801e98e37da9a51b8dc59d7885378
drovvns <patrick.a.jury@gmail.com>
2015-11-23 20:48:30 +0000

    worked up to 5.4 in tutorial

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    context_dict = {'boldmessage': "I am the bold font from the context"}
+    return render(request, 'rango/index.html', context_dict)
+
+
+
+
+
+
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -81,4 +81,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 STATIC_URL = '/static/'
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
+
+TEMPLATE_DIRS = (
+    TEMPLATE_PATH,
+)
+
