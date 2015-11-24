87798783df06d6eb63a0dfdcdebc7d0a920498cf
drovvns <patrick.a.jury@gmail.com>
2015-11-24 21:32:01 +0000

    worked to 7.2.3

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from rango.models import Category
 
 # Create your views here.
 
 def index(request):
-    context_dict = {'boldmessage': "I am the bold font from the context"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+
     return render(request, 'rango/index.html', context_dict)
 
 
