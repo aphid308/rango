b5955bd2f5fc04575d7d2fa8382655945786d7d3
drovvns <patrick.a.jury@gmail.com>
2015-11-25 17:48:55 +0000

    finished ch 7

--- rango/views.py
+++ rango/views.py
@@ -1,17 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
-from rango.models import Category
+from rango.models import Page, Category
 
 # Create your views here.
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
 
     return render(request, 'rango/index.html', context_dict)
 
+def category(request, category_name_slug):
+    context_dict = {}
 
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        pass
+
+    return render(request, 'rango/category.html', context_dict)
 
 
 
