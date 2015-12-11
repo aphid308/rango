baa3905aec1970edcb880203c36eb0f36ddd0fef
Ubuntu <drovvns@drowns02.drowns02.b7.internal.cloudapp.net>
2015-12-11 20:19:33 +0000

    Session cookies for counting visits implemented beginning 11.6

--- rango/views.py
+++ rango/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
 
 # Create your views here.
 
@@ -15,7 +16,39 @@ def index(request):
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
 
-    return render(request, 'rango/index.html', context_dict)
+    visits = int(request.COOKIES.get('visits', '1'))
+
+    reset_last_visit_time = False
+    response = render(request, 'rango/index.html', context_dict)
+
+    #does the cookie, last_visit, exit?
+    if 'last_visit' in request.COOKIES:
+        #it does get the value
+        last_visit = request.COOKIES['last_visit']
+        #cast it as a date time object
+        last_visit_time = datetime.strptime(
+            last_visit[:-7], "%Y-%m-%d %H:%M:%S"
+        )
+
+        #if it's been more than a day since last visit
+        if (datetime.now() - last_visit_time).days > 0:
+            visits = visits + 1
+            #flag up the last visit cookie to be updated
+            reset_last_visit_time = True
+
+    else:
+        #cookie last visit does not exist so flag it to be set
+        reset_last_visit_time = True
+        context_dict['visits'] = visits
+        #obtain our response object early so we can add cookie info
+        response = render(request, 'rango/index.html', context_dict)
+
+    if reset_last_visit_time:
+        response.set_cookie('last_visit', datetime.now())
+        response.set_cookie('visits', visits)
+
+    #return response to user updating any cookies that need to be changed
+    return response
 
 def category(request, category_name_slug):
     context_dict = {}
